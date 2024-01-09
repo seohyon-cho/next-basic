272fb3baf22c1d56d121fb150a8ff507aac72d82
seohyon-cho <flanique@naver.com>
2024-01-09 13:09:07 +0900

    페이지 기반 폴더 라우팅

--- app/gallery/page.jsx
+++ app/gallery/page.jsx
@@ -1,13 +1,12 @@
-import styles from './about.module.scss';
-
+import styles from './gallery.module.scss';
 /*
   next 에서는 기본적으로 app 폴더 안쪽에 폴더명을 라우터 이름으로 설정하고, 
   그 안쪽에 있는 page.jsx 페이지를 해당 라우터명에 연결하는 구조.
 */
-export default function About() {
+export default function Gallery() {
 	return (
-		<main className={styles.about}>
-			<h1>about Page</h1>
+		<main className={styles.gallery}>
+			<h1>gallery Page</h1>
 		</main>
 	);
 }
--- app/layout.jsx
+++ app/layout.jsx
@@ -8,7 +8,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
 	return (
 		<html lang='en'>
-			<body>{children}</body>
+			<body>
+				<header>Menu</header>
+				{children}
+			</body>
 		</html>
 	);
 }
