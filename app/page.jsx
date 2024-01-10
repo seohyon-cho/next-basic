f7d77f328ead7e8612b42c86f87675ce9a7a9c0f
seohyon-cho <flanique@naver.com>
2024-01-10 11:57:03 +0900

    이미지 최적화 진행 (1) (최적화 전, 기본 구조만 생성)

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,14 @@
 import styles from './main.module.scss';
-console.log(styles);
+import clsx from 'clsx';
+import pic from './pic.jpg';
 
 export default function Home() {
 	return (
 		<main className={styles.main}>
 			<h1>main Page</h1>
+			<div className={clsx(styles.pic)}>
+				<img src={pic.src} alt='image' />
+			</div>
 		</main>
 	);
 }
