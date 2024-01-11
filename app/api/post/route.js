6bb379bd297c948283775547970fce87d6dbd0d9
seohyon-cho <flanique@naver.com>
2024-01-11 11:11:40 +0900

    서버에 있는 배열값 응답 처리 (api에서 데이터 뽑아와서 실사용하여 출력)

--- app/api/post/route.js
+++ app/api/post/route.js
@@ -4,6 +4,17 @@
 
 import { NextResponse } from 'next/server';
 
+const posts = [
+	{ name: 'David', age: 20 },
+	{ name: 'Emily', age: 22 },
+	{ name: 'Michael', age: 30 },
+];
+
 export function GET() {
-	return NextResponse.json({ data: '클라이언트 요청에 대한 Post 응답' });
+	return NextResponse.json({ result: posts });
 }
+
+/*
+  1. 서버쪽 API 라우터에서 미리 클라이언트로부터 요청이 들어올 것에 대비한 응답로직 라우팅 구현
+  2. 클라이언트 쪽에서 fetch로 API 요청을 보내면, 미리 설정된 서버쪽 라우터로부터 응답을 받음.
+*/
